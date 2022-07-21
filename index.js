82fe302e96ff04aa4aa12554a90c2924627ef0b0
MEclihpse <ixxryanxxi22@gmail.com>
2022-07-21 19:48:18 +0700

    updated js

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 function cariVarian() {
     const varian = 
     {
@@ -6,38 +5,32 @@ function cariVarian() {
         Delta: [`Sakit kepala`, `Pusing`, `Anosmia`],
         Omicron: [`Tenggorokan kering`, `Gangguan vital`, `Mata merah`]
     }
- let name = document.getElementById("nama")
- let gejala1 = document.getElementById("gejala1").value   
- let gejala2 = document.getElementById("gejala2").value   
- let gejala3 = document.getElementById("gejala3").value 
+ let name = document.getElementById("nama").value
+ let gejala1 = document.getElementById("gejala1").value  
+ let gejala2 = document.getElementById("gejala2").value
+ let gejala3 = document.getElementById("gejala3").value
  let output = document.getElementById("output")
  let countA = 0
  let countD = 0
  let countO = 0
- for (let i in varian.Alpha) {
-     if (gejala1 === varian.Alpha[i]) countA++ 
-     if (gejala2 === varian.Alpha[i]) countA++ 
-     if (gejala3 === varian.Alpha[i]) countA++ 
+    for (let i in varian.Alpha) {
+     if (gejala1 === varian.Alpha[i] || gejala2 === varian.Alpha[i] || gejala3 === varian.Alpha[i]) countA++ 
     }
     for (let j in varian.Delta) {
-        if (gejala1 === varian.Delta[j]) countD++ 
-        if (gejala2 === varian.Delta[j]) countD++ 
-        if (gejala3 === varian.Delta[j]) countD++ 
+        if (gejala1 === varian.Alpha[j] || gejala2 === varian.Alpha[j] || gejala3 === varian.Alpha[j]) countD++  
     }
     for (let k in varian.Omicron) {
-        if (gejala1 === varian.Omicron[k]) countO++ 
-        if (gejala2 === varian.Omicron[k]) countO++ 
-        if (gejala3 === varian.Omicron[k]) countO++ 
+        if (gejala1 === varian.Alpha[k] || gejala2 === varian.Alpha[k] || gejala3 === varian.Alpha[k]) countO++ 
     }
 let arr = [countA, countD, countO].sort()[2]
-if (arr === countA) {
-    output.innerHTML = `${name.innerHTML} anda terkena gejala Covid varian Alpha`
+    if (arr === countA) {
+        output.innerHTML = `${name} anda terkena gejala Covid varian Alpha`
+    }else if (arr === countD) {
+        output.innerHTML = `${name} anda terkena gejala Covid varian Delta`
+    }else {
+        output.innerHTML = `${name} anda terkena gejala Covid varian Omicron`
+    }
 }
-if (arr === countD) {
-    output.innerHTML = `${name.innerHTML} anda terkena gejala Covid varian Delta`
-}
-if (arr === countO) {
-    output.innerHTML = `${name.innerHTML} anda terkena gejala Covid varian Omicron`
-}
-return output
-}
+
+
+
